0721f183d0b47090061494b8a703e6f1702900d8
ShaikAfridh <86220601+ShaikAfridh@users.noreply.github.com>
2022-10-21 09:33:32 +0530

    Add files via upload

---
+++ day1/day1.cpp
@@ -0,0 +1,102 @@
+// 1.I/O
+// #include<bits/stdc++.h> //it will include all header files that required for competitive programming
+
+// using namespace std;
+
+// int main(){ //main is a function our program execution start from here
+// 	cout << "hello"<<"~"<<"my name is afridi";
+	
+// }
+
+// 2.Variables --> store some value in a name
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int main(){
+// 	int a = 2;
+// 	int b = 3;
+// 	int sum=a+b;
+// 	cout<<sum;
+
+// }
+// 3.Datatypes
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int main(){
+// 	//char-->store any single character, int---> stores intergers value(----,-2,-1,0,1,2,----), float--> store decimal value, double--> store decimal number, bool--->store true or false value
+// 	//long int ---> store long integer ,long long int 
+// 	char c='a';
+// 	int a=3;
+// 	double b=3.5;
+// 	a=3.5;
+// 	bool d=false; //false(0) true(1)
+// 	// bool d=123 //true(1)
+// 	// bool d=-123//true(1)
+// 	// bool d=0; //false(0)--> in PL only value 0 is false and except all values are true 
+// 	long int e=1234;
+// 	long long f=123456;
+// 	cout<<c<<" "<<a <<" "<<b<<" "<<d<<" "<<e<<" "<<f<<endl;//endl prints new line
+
+// }
+
+// 4.Operators
+//   Arithemetic operators (+,-,*,/,%,++,--)
+//   Relational Operators{mostly used in if else conditions} (==(checking),!=(not equal to),>(greater than),<(less than),>=(greater than equal to),<=(less than equal to))
+
+//noraml operators
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int main(){
+// 	cout<<2*3<<endl;
+// 	cout<<21%7<<endl;
+
+// }
+//increment and decrement operators
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int main(){
+// 	int a=5;
+// 	a = a+1;
+// 	cout<<a<<endl;
+// 	// a++; //a+1(post increment)
+// 	// ++a; //1+a(pre increment)
+// 	// a--; //a-1(post decrement)
+// 	// --a; //1-a(pre decrement)
+// 	cout<<a++<<endl;
+// 	cout<<a<<endl;
+// 	cout<<++a<<endl;
+// }
+
+// 5.Characters and ASCII value
+// link:- https://www.cs.cmu.edu/~pattis/15-1XX/common/handouts/ascii.html
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int main(){
+// 	char a='c';
+// 	char b='a';
+
+// 	cout<<(int)a<<endl;
+// 	cout<<a-b;//diffeence of ASCII values
+
+
+
+// }
+
+// 6.Taking input from user
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int a;
+	double b;
+	char c;
+	cin>>a>>b>>c;
+	cout<<a<<" "<<b<<" "<<c<<" "<<(int)c<<"\n"; //\n is used to print new line 
+	//in cin file spaces will ignore and takes when values is given
+	
+}
+
